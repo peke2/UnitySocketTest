1fee9b37fa8788409b789490c1354cfba1a8affb
peke2 <abem@dc4.so-net.ne.jp>
2018-04-11 11:24:38 +0900

    画面解像度の変更を試してみた。使わないのでコメントアウトしている。 改行コードをLFに修正。

--- Assets/Scripts/Info.cs
+++ Assets/Scripts/Info.cs
@@ -8,6 +8,18 @@ public class Info : MonoBehaviour {
 
 	public Text myIpAddressText = null;
 
+	void Awake()
+	{
+		//	画面サイズの変更テスト
+		/*
+		float aspect = (float)Screen.width / Screen.height;
+		int w = 800;
+		int h = (int)(w / aspect);
+		Screen.SetResolution(w, h, true);
+		*/
+	}
+
+
 	// Use this for initialization
 	void Start ()
 	{
